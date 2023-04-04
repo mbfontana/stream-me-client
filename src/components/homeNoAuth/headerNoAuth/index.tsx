7e4e6ed98ee3ec324a544e584c41c61f3c8240a2
Matheus Fontana <mbfontana@outlook.com>
2023-04-03 22:28:09 -0300

    Header and responsiveness

--- src/components/homeNoAuth/headerNoAuth/index.tsx
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -1,31 +1,39 @@
 import { Container, Button } from "reactstrap";
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 const HeaderNoAuth = () => {
   return (
     <>
       <div className={styles.ctaHeaderTop}>
-        <CtaImage />
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="Logo CTA"
+          className={styles.ctaImageTop}
+        />
         <p>Register to have access to the courses</p>
-        <CtaImage />
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="Logo CTA"
+          className={styles.ctaImageTop}
+        />
       </div>
-      <Container>
-        <img src="./logo.svg" alt="logo" />
+      <Container className={styles.nav}>
+        <img src="./logo.svg" alt="logo" className={styles.navLogo} />
         <div>
-          <Button>Sign-in</Button>
+          <Link href="/login">
+            <Button className={styles.navButton} outline>
+              Sign-in
+            </Button>
+          </Link>
+          <Link href="/register">
+            <Button className={styles.navButton} outline>
+              Get Started
+            </Button>
+          </Link>
         </div>
       </Container>
     </>
-  );
-};
-
-const CtaImage = () => {
-  return (
-    <img
-      src="/homeNoAuth/logoCta.png"
-      alt="Logo CTA"
-      className={styles.ctaImageTop}
-    />
   );
 };
 
