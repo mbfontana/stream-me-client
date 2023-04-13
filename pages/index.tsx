c6d18fde0fd72f39ce7266ede3fafee5123add29
Matheus Fontana <mbfontana@outlook.com>
2023-04-13 20:04:31 -0300

    HomeNoAuth first section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
 import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
+import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
 
 const HomeNoAuth = () => {
   return (
@@ -13,6 +14,7 @@ const HomeNoAuth = () => {
       </Head>
       <main>
         <HeaderNoAuth />
+        <PresentationSection />
       </main>
     </>
   );
---
+++ src/components/homeNoAuth/presentationSection/index.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { Button, Container, Row, Col } from "reactstrap";
+
+const PresentationSection = () => {
+  return (
+    <>
+      <Container className="py-5">
+        <Row className={styles.presentationRow}>
+          <Col md>
+            <p className={styles.subtitle}>ILIMITED ACCESS</p>
+            <p className={styles.title}>
+              Give your carrer a boost with <br /> our top courses courses
+            </p>
+            <p className={styles.description}>
+              Study everywhere, anytime, and take your skills <br /> to the next
+              level
+            </p>
+            <p>
+              <Link href="/register" className="btnLink">
+                <Button outline className={styles.btnCta}>
+                  ACCESS NOW
+                  <img
+                    src="./buttonPlay.svg"
+                    alt="CTA Button"
+                    className={styles.btnImg}
+                  />
+                </Button>
+              </Link>
+            </p>
+          </Col>
+          <Col md>
+            <img
+              src="./homeNoAuth/imgPresentation.png"
+              alt="Presentation Image"
+              className={styles.imgPresentation}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col className={styles.arrowDownCol}>
+            <img
+              src="./homeNoAuth/iconArrowDown.svg"
+              alt="Scroll Arrow Down"
+              className={styles.arrowDown}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default PresentationSection;
