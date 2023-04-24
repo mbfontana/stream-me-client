9921c0f8aceaa2743bef542863bc3795854d5f39
Matheus Fontana <mbfontana@outlook.com>
2023-04-24 19:56:13 -0300

    API profile service

---
+++ src/services/profileService.ts
@@ -0,0 +1,46 @@
+import api from "./api";
+
+export type UserParams = {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  createdAt: string;
+}
+
+const profileService = {
+  getCurrentUser: async () => {
+    const token =
+      localStorage.getItem("streamMe-token") ||
+      sessionStorage.getItem("streamMe-token");
+
+    const res = await api
+      .get("/users/account", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+
+    return res.data;
+  },
+  updateUser: async (params: UserParams) => {
+    const token =
+      localStorage.getItem("streamMe-token") ||
+      sessionStorage.getItem("streamMe-token");
+
+    const res = await api
+      .put("/users/account", params, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .catch((error) => {
+        if (error.response.status === 400 || error.response.status === 401)
+          return error.response;
+        return error;
+      });
+
+    return res.status;
+  },
+};
+
+export default profileService;
