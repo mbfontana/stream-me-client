22f92319ee19e1ad31bcad1a21488e067e541a64
Matheus Fontana <mbfontana@outlook.com>
2023-04-24 01:33:30 -0300

    Cateogry Service with two methods (getAll, getOneById)

---
+++ src/services/categoryService.ts
@@ -0,0 +1,44 @@
+import api from "./api";
+import { CourseType } from "./courseService";
+
+export type CategoryType = {
+  id: number;
+  name: string;
+  position: number;
+  courses?: CourseType[];
+};
+
+const categoryService = {
+  getAllCategories: async () => {
+    const token =
+      localStorage.getItem("streamMe-token") ||
+      sessionStorage.getItem("streamMe-token");
+
+    const res = await api
+      .get("/categories", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+
+    return res;
+  },
+  getOneById: async (id: number) => {
+    const token =
+      localStorage.getItem("streamMe-token") ||
+      sessionStorage.getItem("streamMe-token");
+
+    const res = await api
+      .get(`/categories/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+
+    return res;
+  },
+};
+
+export default categoryService;
