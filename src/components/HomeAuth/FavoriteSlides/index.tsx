be78ace444684fb0b8eb36eca0416c3ec4dd8601
Matheus Fontana <mbfontana@outlook.com>
2023-04-24 01:08:03 -0300

    Created Favorite Slides and added to HomeAuth

---
+++ src/components/HomeAuth/FavoriteSlides/index.tsx
@@ -0,0 +1,29 @@
+import useSWR from "swr";
+import styles from "./styles.module.scss";
+import { Container } from "reactstrap";
+import Slides from "../../common/Slides";
+import courseService from "@/src/services/courseService";
+
+const FavoriteSlides = () => {
+  const { data, error } = useSWR("/favorites", courseService.getFavorites);
+
+  if (error) return error;
+  if (!data)
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+  return (
+    <Container fluid className="d-flex flex-column align-items-center">
+      <p className={styles.title}>MY LIST</p>
+      {data.data.courses.length > 1 ? (
+        <Slides courses={data.data.courses} />
+      ) : (
+        <p className="py-2">Try to add courses to your favorites list</p>
+      )}
+    </Container>
+  );
+};
+
+export default FavoriteSlides;
